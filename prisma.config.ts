c07d0d3276b08b461033348adcc0412fe0359b73
PhenomenalBen <benriak1@gmail.com>
2026-05-13 07:47:22 +0000

    Remove directUrl from prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,7 +3,6 @@ import 'dotenv/config'
 
 export default defineConfig({
   datasource: {
-    url: process.env.DATABASE_URL!,
-    directUrl: process.env.DIRECT_URL,
+    url: process.env.DATABASE_URL!
   },
 })
